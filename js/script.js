8a108a38f7913a46d8fa5bb82e463cb322df2160
ejevichkin <88722239+ejevichkin@users.noreply.github.com>
2022-02-02 17:55:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,105 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.header__menu'),
-    menuItem = document.querySelectorAll('.header__menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('header__menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('header__menu_active');
-        });
-    });
-});
-
 $(document).ready(function(){
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    $('.carousel__inner').slick(
+        {
+        speed: 1200,
+        adaptiveHeight: true,   
+        autoplay: true,
+        autoplayspeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg"></button>'    
+
     });
-
-    function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
-        });
-    };
-
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
-
-$('input[name=phone').mask("+7(999)999-99-99");
-
-//modal
-
-    // $('[data-modal="thanks"]').on('click',function() {
-    //     $('.overlay, #thanks').fadeIn('slow');
-    // }); 
-    $('.modal__close').on('click',function(){
-        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
-    });
-
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function(){
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
-        })
-    });
-
-    function validateForms(form){
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                phone: "required",
-                email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                name: {
-                    required: "Введите название лекарства",
-                    minlength: jQuery.validator.format("Введите {0} символа!")
-                  },
-                phone: "Пожалуйста, введите Ваш номер телефона",
-                
-            }
-        });
-    };
-
-    validateForms('#consultation-form');
-    validateForms('#call_me');
-    validateForms('#order form');
-
-    $('form').submit(function(e) {
-        e.preventDefault();
-
-        if (!$(this).value()) {
-            return;
-        }
-        $.ajax({
-            type: "POST",
-            url: "mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-            $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
-
-            $('form').trigger('reset');
-        });
-        return false;
-    });
-    
-}); 
+ });
