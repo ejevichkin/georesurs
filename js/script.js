dddf35702f30fc802ad9b178b79db34e1f7dadfa
ejevichkin <88722239+ejevichkin@users.noreply.github.com>
2022-02-03 17:11:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ $(document).ready(function(){
 
     });
  });
+ window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    });
+});
